b9d191efea4da0740e53ac0e1cb703f6e5031e2e
David Alsh <alshdavid@gmail.com>
2023-07-16 13:05:45 +1000

    prettier

--- tests/utils.mjs
+++ tests/utils.mjs
@@ -1,55 +1,51 @@
-import * as path from "node:path";
-import * as fs from "node:fs";
-import * as fsAsync from "node:fs/promises";
+import * as path from 'node:path'
+import * as fs from 'node:fs'
+import * as fsAsync from 'node:fs/promises'
 
 function __dirname() {
-  if (process.platform === "win32") {
-    return path.dirname(import.meta.url).replace("file:///", "");
+  if (process.platform === 'win32') {
+    return path.dirname(import.meta.url).replace('file:///', '')
   }
-  return path.dirname(import.meta.url).replace("file://", "");
+  return path.dirname(import.meta.url).replace('file://', '')
 }
 
 export function __reportdir() {
-  return path.join(__dirname(), "reports");
+  return path.join(__dirname(), 'reports')
 }
 
 export function loadCases() {
-  const __fixtures = path.join(__dirname(), "fixtures");
+  const __fixtures = path.join(__dirname(), 'fixtures')
 
   /** @type {[string, string, number][]} */
-  const cases = [];
+  const cases = []
   for (const caseDir of fs.readdirSync(__fixtures)) {
-    cases.push([
-      path.join(__fixtures, caseDir),
-      caseDir,
-      parseInt(caseDir, 10),
-    ]);
+    cases.push([path.join(__fixtures, caseDir), caseDir, parseInt(caseDir, 10)])
   }
-  return cases;
+  return cases
 }
 
 export async function readFile(/** @type {string[]} */ ...filepathSegments) {
-  return await fsAsync.readFile(path.join(...filepathSegments), "utf8");
+  return await fsAsync.readFile(path.join(...filepathSegments), 'utf8')
 }
 
 export async function writeFile(
   /** @type {string} */ content,
-  /** @type {string[]} */ filepathSegments,
+  /** @type {string[]} */ filepathSegments
 ) {
-  const targetFile = path.join(...filepathSegments);
+  const targetFile = path.join(...filepathSegments)
   if (!fs.existsSync(path.dirname(targetFile))) {
-    fs.mkdirSync(path.dirname(targetFile), { recursive: true });
+    fs.mkdirSync(path.dirname(targetFile), { recursive: true })
   }
-  await fsAsync.writeFile(targetFile, content, "utf8");
+  await fsAsync.writeFile(targetFile, content, 'utf8')
 }
 
 export async function readJson(/** @type {string[]} */ ...filepathSegments) {
-  return JSON.parse(await readFile(...filepathSegments));
+  return JSON.parse(await readFile(...filepathSegments))
 }
 
 export async function writeJson(
   /** @type {any} */ json,
-  /** @type {string[]} */ filepathSegments,
+  /** @type {string[]} */ filepathSegments
 ) {
-  await writeFile(JSON.stringify(json, null, 2), filepathSegments);
+  await writeFile(JSON.stringify(json, null, 2), filepathSegments)
 }
